e6ec7799576d16b560c0c786c7f65f1632667f04
bellmao <panbotoy@gmail.com>
2014-10-16 23:40:18 -0700

    initial commit;

---
+++ ParseStarterProject/ParseStarterProjectViewController.m
@@ -0,0 +1,33 @@
+//
+//  ParseStarterProjectViewController.m
+//  ParseStarterProject
+//
+//  Copyright 2014 Parse, Inc. All rights reserved.
+//
+
+#import "ParseStarterProjectViewController.h"
+
+#import <Parse/Parse.h>
+
+@implementation ParseStarterProjectViewController
+
+#pragma mark - UIViewController
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
