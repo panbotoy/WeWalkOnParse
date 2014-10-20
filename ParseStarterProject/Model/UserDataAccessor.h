c76c840b6fb2a254de2696fa084d681c19f855c5
Stella Mao <Stella@Stellas-MacBook-Pro.local>
2014-10-19 23:46:01 -0700

    Commit - add the model and data accessor layer

---
+++ ParseStarterProject/Model/UserDataAccessor.h
@@ -0,0 +1,13 @@
+//
+//  UserDataAccessor.h
+//  ParseStarterProject
+//
+//  Created by Stella Mao on 10/19/14.
+//
+//
+
+#import "WeWalkDataAccessor.h"
+
+@interface UserDataAccessor : WeWalkDataAccessor
+
+@end
---
+++ ParseStarterProject/Model/WeWalkDataAccessor.m
@@ -0,0 +1,13 @@
+//
+//  WeWalkDataAccessor.m
+//  ParseStarterProject
+//
+//  Created by Stella Mao on 10/19/14.
+//
+//
+
+#import "WeWalkDataAccessor.h"
+
+@implementation WeWalkDataAccessor
+
+@end
