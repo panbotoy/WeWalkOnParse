d61e3c22c787e988e9dec23537c1751e74898753
Stella Mao <Stella@Stellas-MacBook-Pro.local>
2014-10-19 22:53:43 -0700

    Bug#1 fixed commit

--- ParseStarterProject/Controller/WeWalkSignInController.m
+++ ParseStarterProject/Controller/WeWalkSignInController.m
@@ -14,6 +14,7 @@ UIScrollViewDelegate,
 WeWalkSignInControllerDelegate>
 
 @property (strong, nonatomic) IBOutlet UIButton *loginButton;
+@property (strong, nonatomic) UITapGestureRecognizer *tapRecognizer;
 
 @end
 
@@ -33,7 +34,16 @@ WeWalkSignInControllerDelegate>
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    
+    [nc addObserver:self selector:@selector(keyboardWillShow:) name:
+     UIKeyboardWillShowNotification object:nil];
+    
+    [nc addObserver:self selector:@selector(keyboardWillHide:) name:
+     UIKeyboardWillHideNotification object:nil];
+    
+    self.tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                action:@selector(didTapAnywhere:)];
 }
 
 - (void)didReceiveMemoryWarning
@@ -48,6 +58,20 @@ WeWalkSignInControllerDelegate>
 - (IBAction)signInButtonPressed:(UIButton *)sender {
     
     
+}
+
+-(void) keyboardWillShow:(NSNotification *) note {
+    [self.view addGestureRecognizer:self.tapRecognizer];
+}
+
+-(void) keyboardWillHide:(NSNotification *) note
+{
+    [self.view removeGestureRecognizer:self.tapRecognizer];
+}
+
+-(void)didTapAnywhere: (UITapGestureRecognizer*) recognizer {
+    [self.usernameField resignFirstResponder];
+    [self.passwordField resignFirstResponder];
 }
 
 @end
